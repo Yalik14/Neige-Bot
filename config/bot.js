fb91ad5cd02954d036f2b104c32a48dfcad6848c
Yalik14 <84687190+Yalik14@users.noreply.github.com>
2021-05-24 00:26:07 +0200

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODQ2MTQ0MDczMDg5MTU1MDcy.YKrPEw.G-hCAGFqySIIytrp9oyajICEJxA',
+        prefix: '!',
+        activity: 'ANIMES',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
